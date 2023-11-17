5ba7f8745e0b49c4ddee1551b55e7613ec7244b8
sam0hack <sam.nyx@live.com>
2023-11-17 05:55:20 +0530

    Html Mail template

---
+++ resources/views/emails/new_achievement.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Achievement Unlocked</title>
+</head>
+<body>
+<h1>Congratulations, {{ $userName }}!</h1>
+<p>You have unlocked a new achievement: <strong>{{ $achievementTitle }}</strong>.</p>
+<p>Keep up the great work!</p>
+</body>
+</html>
---
+++ resources/views/emails/new_badge.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Badge Unlocked</title>
+</head>
+<body>
+<h1>Congratulations, {{ $userName }}!</h1>
+<p>You have unlocked a new Badge: <strong>{{ $badgeTitle }}</strong>.</p>
+<p>Keep up the great work!</p>
+</body>
+</html>
