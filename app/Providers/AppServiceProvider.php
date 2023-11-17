abcd8d6e47bf5c2886fd2bb81f65892e8f53579f
sam0hack <sam.nyx@live.com>
2023-11-17 05:48:01 +0530

    📦 Register Achievements and Badges repositories as singletons in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\Achievements\AchievementsRepository;
+use App\Repositories\Achievements\AchievementsRepositoryInterface;
+use App\Repositories\Achievements\BadgesRepository;
+use App\Repositories\Achievements\BadgesRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(AchievementsRepositoryInterface::class, AchievementsRepository::class);
+        $this->app->singleton(BadgesRepositoryInterface::class,BadgesRepository::class);
     }
 
     /**
