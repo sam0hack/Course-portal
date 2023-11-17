cac6ff9adc02b2def600003980f0b1f303976512
sam0hack <sam.nyx@live.com>
2023-11-17 05:43:07 +0530

    🏭 added LessonUserFactory for improved data generation

---
+++ database/factories/LessonUserFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lesson;
+use App\Models\LessonUser;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class LessonUserFactory extends Factory
+{
+    protected $model = LessonUser::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'lesson_id' => Lesson::factory(),
+            'watched' => 1
+        ];
+    }
+}
