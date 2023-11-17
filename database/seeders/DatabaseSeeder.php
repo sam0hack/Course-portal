4f0ad3fcdd82f5175d06119cabd5ec4e98bcebfb
sam0hack <sam.nyx@live.com>
2023-11-17 05:42:18 +0530

    clean

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $lessons = Lesson::factory()
-            ->count(20)
-            ->create();
+        
     }
 }
